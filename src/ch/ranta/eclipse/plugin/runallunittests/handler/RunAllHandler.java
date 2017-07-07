2d187ed311d99a4ea7220cc1ec23a964e22a57b8
LukyLuke <lukas@delightsoftware.com>
2017-07-07 12:07:03 +0200

    remove toolbar and move to run menu

--- src/ch/ranta/eclipse/plugin/runallunittests/handler/RunAllHandler.java
+++ src/ch/ranta/eclipse/plugin/runallunittests/handler/RunAllHandler.java
@@ -4,7 +4,6 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 
 import ch.ranta.eclipse.plugin.runallunittests.Activator;
@@ -13,22 +12,8 @@ public class RunAllHandler {
 	
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_SHELL) Shell shell) {
-		String title = "E4 Information Dialog";
-		String message = "Hello world from a pure Eclipse 4 plug-in";
-		
 		Activator plugin = Activator.getPlugin();
 		plugin.runAllTests();
-		
-		// org.eclipse.ui.edit.text.actionSet.annotationNavigation
-		// org.eclipse.jdt.junit.jJUiitActionSet
-		
-		showDialog(shell, title, message);
 	}
-
-	void showDialog(Shell s, String title, String message) {
-		MessageDialog.openInformation(s, title, message);
-	}
-
-	
 	
 }
